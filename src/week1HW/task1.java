15bda358baa5aad4078c39ca359b487ec567ed6c
presly808 <presly808@gmail.com>
2016-03-14 15:33:30 +0200

    Update task1.java

--- src/week1HW/task1.java
+++ src/week1HW/task1.java
@@ -6,13 +6,19 @@ import java.util.Scanner;
 public class task1 {
 
     public static void main(String[] args) {
+        //TODO make sections by breacking line
         Scanner sc = new Scanner(System.in);
+        
         System.out.println("Please, enter current time:");
         int time = sc.nextInt();
+        
+        //validation for incorrect input data like a -56
         if (time >= 9 && time <= 18){
             System.out.println("I am at work");
         } else {
             System.out.println("I am resting");
         }
+        
+        
     }
 }
